236ab4b5358190e5aa8955ee87dedaa9bd60917e
JatinChauhan1998 <jtnchauhan24@gmail.com>
2018-02-02 07:26:20 +0530

    fixed some errors and typos

--- src/1-datatype.r
+++ src/1-datatype.r
@@ -2,6 +2,9 @@
 name<-"Hello, World!"
 print(name)
 
+#removing an object from the working directory
+rm(name)
+
 #R- Data Types
 "The variables are assigned with R-Objects and the data type of the R-object 
 becomes the data type of the variable. There are many types of R-objects. 
--- src/22-lists.r
+++ src/22-lists.r
@@ -13,8 +13,7 @@ print(list_data)
 "Naming List Elements"
 #The list elements can be given names and they can be accessed using these names.
 # Create a list containing a vector, a matrix and a list.
-list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2),
-   list("green",12.3))
+list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2),list("green",12.3))
 # Give names to the elements in the list.
 names(list_data) <- c("1st Quarter", "A_Matrix", "A Inner list")
 # Show the list.
